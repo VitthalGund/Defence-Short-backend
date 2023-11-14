3cb976e5899d1bf368f808da66c53960379f40b7
VitthalGund <vitthalgund30@gmail.com>
2023-11-14 10:30:51 +0530

    user management controller is added for admin control

---
+++ controllers/usersController.js
@@ -0,0 +1,38 @@
+const User = require('../model/User');
+
+//These are some functions for admin only
+
+// function to get All the users registered in database
+const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    if (!users) return res.status(204).json({ 'message': 'No users found' });
+    res.json(users);
+}
+
+
+// to delete particular user from the database
+const deleteUser = async (req, res) => {
+    if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.body.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+    }
+    const result = await user.deleteOne({ _id: req.body.id });
+    res.json(result);
+}
+
+// to get the information of particular user
+const getUser = async (req, res) => {
+    if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.params.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
+    }
+    res.json(user);
+}
+
+module.exports = {
+    getAllUsers,
+    deleteUser,
+    getUser
+}
