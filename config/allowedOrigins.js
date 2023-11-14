be3656f763eef5b8d0d03270211b482260d10551
VitthalGund <vitthalgund30@gmail.com>
2023-11-14 10:25:16 +0530

    backend allowed origins added

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'https://www.defenceshorts.com',
+    'http://127.0.0.1:5500',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:3200'
+];
+
+module.exports = allowedOrigins;
